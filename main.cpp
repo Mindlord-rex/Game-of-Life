e79f1c5870db014ca0c4334cb78e7ed2265463eb
Manish Kumar <manish123hello@gmail.com>
2024-07-10 19:53:37 +0530

    error revoked

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 class GameOfLife{
+public:
     vector<vector<int>> random_state(int width, int height){
         vector<vector<int>> state(width, vector<int> (height, 0));
 
@@ -18,11 +19,14 @@ class GameOfLife{
         return vector<vector<int>> (width, vector<int> (height, 0));
     }
 
-    int main(){
+};
+
+int main(){
+        GameOfLife game;
         int width = 10;
         int height = 10;
-        vector<vector<int>> state = random_state(width, height);
-        vector<vector<int>> new_state = dead_state(width, height);
+        vector<vector<int>> state = game.random_state(width, height);
+        vector<vector<int>> new_state = game.dead_state(width, height);
 
         for(int i=0; i<width; i++){
             for(int j=0; j<height; j++){
@@ -32,5 +36,4 @@ class GameOfLife{
         }
 
         return 0;
-    }
-};
+}
