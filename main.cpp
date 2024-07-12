2637a4c0b8a0c589825fe0ce0b78a6d44369fb60
Manish Kumar <manish123hello@gmail.com>
2024-07-12 23:50:52 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ class GameOfLife{
 public:
     vector<vector<int>> random_state(int width, int height){
         vector<vector<int>> state = dead_state(width, height);
-
+        srand(time(NULL));
         for(int i=0; i<width; i++){
             for(int j=0; j<height; j++){
                 state[i][j] = rand()%2;
@@ -22,8 +22,16 @@ public:
     vector<vector<int>> next_board_state(vector<vector<int>> state){
         for(int i=0;i<state[0].size();i++){
             for(int j=0;j<state.size();j++){
-                int sum = state[i-1][j-1] + state[i-1][j] + state[i-1][j+1] + state[i][j-1] +state[i][j+1] +state[i+1][j-1] + state[i+1][j]+ state[i+1][j+1];
+                int sum = 0;
 
+                if(i!=0) sum+= state[i-1][j];
+                if(j!=0) sum+= state[i][j-1];
+                if(i!=0 && j!=0) sum+= state[i-1][j-1];
+                if(i!=0 && j!=(state.size()-1)) sum += state[i-1][j+1];
+                if(j!=(state.size()-1)) sum += state[i][j+1];
+                if(i!=(state[0].size()-1) && j!=0) sum+= state[i+1][j-1];
+                if(i!=(state[0].size()-1)) sum+= state[i+1][j];
+                if(i!=(state[0].size()-1) && j!=(state.size()-1)) sum+= state[i+1][j+1];
 
                 if(sum<=1) state[i][j] = 0;
                 else if(sum>3) state[i][j] = 0;
